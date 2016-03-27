fcc5737af6138e9e8cb2ca4aabf444059e6c2155
Michael Zhestkov <michaelzhestkov@gmail.com>
2016-03-27 13:43:06 -0700

    Add .gitignore and java file

---
+++ src/main/java/HomeWorkJogger1.java
@@ -0,0 +1,33 @@
+/**
+ *   File Name: HomeWorkJogger1.java<br>
+ *
+ *   Zhestkov, Michael<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Mar 27, 2016
+ *   
+ */
+
+/**
+ * HomeWorkJogger1 //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Zhestkov, Michael
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class HomeWorkJogger1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
