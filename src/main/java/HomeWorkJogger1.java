d322260935fbcfec2d54b25b009154c74e83e4e6
Michael Zhestkov <michaelzhestkov@gmail.com>
2016-03-27 18:15:40 -0700

    Add First version of homework (unfinished)

--- src/main/java/HomeWorkJogger1.java
+++ src/main/java/HomeWorkJogger1.java
@@ -1,32 +1,47 @@
-/**
- *   File Name: HomeWorkJogger1.java<br>
- *
- *   Zhestkov, Michael<br>
- *   Java Boot Camp Exercise<br>
- *   Instructor: Jean-francois Nepton<br>
- *   Created: Mar 27, 2016
- *   
- */
+import java.util.*;
 
-/**
- * HomeWorkJogger1 //ADDD (description of class)
- * <p>
- * //ADDD (description of core fields)
- * <p>
- * //ADDD (description of core methods)
- * 
- * @author      Zhestkov, Michael
- * @version     1.0.0
- * @since       1.0
- *
- */
 public class HomeWorkJogger1 {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		String userName = null;
+		String input = null;
+		int daysJogged = 0;
+		int x = 1;
+		int hoursJogged = 0;
+		int totalHoursJogged = 0;
+		int avarageHoursJogged = 0;
+
+		System.out.println("Please, enter your name?");
+		userName = scanner.nextLine();
+
+		System.out.println("Hello, " + userName + ". Welcome!");
+
+		System.out.println(userName + ", please tell me how many days have you logged or jogged this week?");
+		daysJogged = scanner.nextInt();
+
+		System.out.println("Thanks! How many hours you have jogged or logged each day?");
+
+		while (x <= daysJogged) {
+			System.out.println("Please, enter how many hours have you spent jogging in your " + x + " day");
+			hoursJogged = scanner.nextInt();
+			x++;
+			totalHoursJogged += hoursJogged;
+			avarageHoursJogged = totalHoursJogged / daysJogged;
+		}
+
+		System.out.println("Confratulations, " + userName + "! Your total number of hours jogging this week is "
+				+ totalHoursJogged);
+		System.out.println("By the way, your avarage number of hours per day is " + avarageHoursJogged);
+
+		if (totalHoursJogged < 15) {
+			System.out.println("You are an Amateur for now. Please, keep working out!");
+		} else if (totalHoursJogged >= 15 && totalHoursJogged < 40) {
+			System.out.println("You are an Efficient Jogger. Good Job!");
+		} else {
+			System.out.println("You are an Extremist Jogger! Amazing!");
+		}
 
 	}
 
